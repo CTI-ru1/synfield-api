1c076773df5b6cae8412f2ac1ad66cf0eb7fde51
Dimitrios Amaxilatis <d.amaxilatis@gmail.com>
2018-02-23 13:01:27 +0200

    add v2 staging api

--- src/main/java/gr/cti/ru1/synfield/client/Synfield.java
+++ src/main/java/gr/cti/ru1/synfield/client/Synfield.java
@@ -1,9 +1,14 @@
 package gr.cti.ru1.synfield.client;
 
-import gr.cti.ru1.synfield.client.model.measurements.SynfieldMeasurementsPage;
-import gr.cti.ru1.synfield.client.model.sensors.SynfieldSensorsPage;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import gr.cti.ru1.synfield.client.model.v1.measurements.SynfieldMeasurementsPage;
+import gr.cti.ru1.synfield.client.model.v1.sensors.SynfieldSensorsPage;
+import gr.cti.ru1.synfield.client.model.v2.Field;
+import gr.cti.ru1.synfield.client.model.v2.LatestMeasurement;
+import gr.cti.ru1.synfield.client.model.v2.Measurements;
+import gr.cti.ru1.synfield.client.model.v2.Node;
+import gr.cti.ru1.synfield.client.model.v2.SensingServices;
 import org.apache.commons.codec.binary.Base64;
-import org.codehaus.jackson.map.ObjectMapper;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -21,7 +26,7 @@ public final class Synfield {
      * LOGGER.
      */
     private static final org.apache.log4j.Logger LOGGER = org.apache.log4j.Logger.getLogger(Synfield.class);
-
+    
     /**
      * The basic authentication token form Synfield app.
      */
@@ -29,41 +34,67 @@ public final class Synfield {
     /**
      * Base url of the Synfield API, by default the testing server is used.
      */
-    private String baseUrlString = "http://api-synfield.synelixis.com";
-
-
+    private String baseUrlv1 = "http://api-synfield.synelixis.com";
+    private String baseUrlv2 = "https://staging.synfield.synelixis.com/en/api/v1/";
+    
+    private final ObjectMapper mapper = new ObjectMapper();
+    
     /**
      * Default Constructor.
      */
     public Synfield() {
         basiscAuthenticationToken = null;
     }
-
+    
     /**
      * <p>Creates a Synfield Api for the given base url.</p>
      *
-     * @param baseUrl The base url to use for the API calls.
+     * @param baseUrlv1 The base url to use for the v1 API calls.
+     * @param baseUrlv2 The base url to use for the v2 API calls.
      */
-    public Synfield(final String baseUrl) {
-        this.baseUrlString = baseUrl;
+    public Synfield(final String baseUrlv1, final String baseUrlv2) {
+        this.baseUrlv1 = baseUrlv1;
+        this.baseUrlv2 = baseUrlv2;
         basiscAuthenticationToken = null;
     }
-
+    
+    //V1
     public SynfieldSensorsPage getSensors(final String mac) throws IOException {
-        final String responseString = getPage("/v1/gateway/" + mac + "/sensors/");
+        final String responseString = getPagev1("/v1/gateway/" + mac + "/sensors/");
         return new ObjectMapper().readValue(responseString, SynfieldSensorsPage.class);
     }
-
+    
     public SynfieldMeasurementsPage getMeasurements(final String mac) throws IOException {
-        final String responseString = getPage("/v1/gateway/" + mac + "/measurements/");
+        final String responseString = getPagev1("/v1/gateway/" + mac + "/measurements/");
         return new ObjectMapper().readValue(responseString, SynfieldMeasurementsPage.class);
     }
-
+    
     public SynfieldMeasurementsPage getMeasurements(final String mac, final String startDate, final String endDate) throws IOException {
-        final String responseString = getPage("/v1/gateway/" + mac + "/measurements/" + startDate + "/" + endDate + "/?limit=2000");
+        final String responseString = getPagev1("/v1/gateway/" + mac + "/measurements/" + startDate + "/" + endDate + "/?limit=2000");
         return new ObjectMapper().readValue(responseString, SynfieldMeasurementsPage.class);
     }
-
+    
+    //V2
+    public Field[] getFields() throws IOException {
+        final String responseString = getPagev2("/fields");
+        return mapper.readValue(responseString, Field[].class);
+    }
+    
+    public SensingServices[] getNodeSensingServices(Node node) throws IOException {
+        final String responseString = getPagev2("/nodes/" + node.getSerialNumber() + "/sensing_services");
+        return mapper.readValue(responseString, SensingServices[].class);
+    }
+    
+    public LatestMeasurement[] getNodeLatestMeasurements(Node node) throws IOException {
+        final String responseString = getPagev2("/nodes/" + node.getSerialNumber() + "/measurements/latest");
+        return mapper.readValue(responseString, LatestMeasurement[].class);
+    }
+    
+    public Measurements getNodeMeasurements(Node node, String from, String to) throws IOException {
+        final String responseString = getPagev2("/nodes/" + node.getSerialNumber() + "/measurements/" + from + "/" + to);
+        return mapper.readValue(responseString, Measurements.class);
+    }
+    
     /**
      * Authenticates the user.
      *
@@ -75,9 +106,7 @@ public final class Synfield {
         final String plainToken = new StringBuilder().append(username).append(":").append(password).toString();
         basiscAuthenticationToken = new String(Base64.encodeBase64(plainToken.getBytes(Charset.forName("UTF-8"))));
     }
-
-//    }
-
+    
     /**
      * <p>Do a GET request for the given API path and return the response as a String.</p>
      *
@@ -85,14 +114,17 @@ public final class Synfield {
      * @return The API call response as a String
      * @throws java.io.IOException if a connection cannot be established
      */
-    private String getPage(final String path) throws IOException {
+    private String getPagev1(final String path) throws IOException {
+        return getPage(baseUrlv1, path);
+    }
+    
+    private String getPagev2(final String path) throws IOException {
+        return getPage(baseUrlv2, path);
+    }
+    
+    private String getPage(final String baseUrl, final String path) throws IOException {
         final Client client = ClientBuilder.newClient();
-        final javax.ws.rs.core.Response response = client.target(baseUrlString)
-                .path(path)
-                .request(MediaType.APPLICATION_JSON_TYPE)
-                .header("Authorization", "Basic " + basiscAuthenticationToken)
-                .header("Accept", "application/json")
-                .get();
+        final javax.ws.rs.core.Response response = client.target(baseUrl).path(path).request(MediaType.APPLICATION_JSON_TYPE).header("Authorization", "Basic " + basiscAuthenticationToken).header("Accept", "application/json").get();
         LOGGER.debug("status: " + response.getStatus());
         return response.readEntity(String.class);
     }
